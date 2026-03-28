cb0bc4c2e820d0e8bed0be97e03720c825cb6f2b
marym0raes <moraesmariana417@gmail.com>
2026-03-27 22:14:01 -0300

    socorro

---
+++ aula01.py
@@ -0,0 +1,16 @@
+from PPlay.window import *
+
+from PPlay.sprite import *
+
+
+janela = Window(2000, 1500)
+janela.set_title("Exercício 2")
+janela.set_background_color((255, 0, 255))
+
+bolinhe = Sprite('bolaa.png')
+bolinhe.x, bolinhe.y = 1000, 500
+
+while True:
+    bolinhe.x, bolinhe.y = (janela.width/2)- (bolinhe.width/2), (janela.height/2) - (bolinhe.height/2)
+    bolinhe.draw()
+    janela.update()
